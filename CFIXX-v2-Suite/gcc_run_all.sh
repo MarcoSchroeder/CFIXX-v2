ea1f3db0e76e00791792844abe35527a131bd39b
Marco Schroeder <marco.schroeder@protonmail.com>
2022-06-08 11:40:44 +0200

    Add build script for gcc

---
+++ CFIXX-v2-Suite/gcc_run_all.sh
@@ -0,0 +1,11 @@
+g++ src/main_all.cpp src/attack_dynamic_dispatch.cpp src/attack_dynamic_cast.cpp src/attack_typeid.cpp -o build/cfixx_clang_all -O2 -g -std=c++11
+
+if [ $? -eq 0 ]
+then
+  echo "Build success. Running executable..."
+  echo ""
+  ./build/cfixx_clang_all
+else
+  echo "Build failure. Aborting..." >&2
+  exit 1
+fi
