7347f03f49de20e0eb062756b2b38dd235dc8e05
Marco Schroeder <marco.schroeder@protonmail.com>
2022-06-08 11:42:05 +0200

    Link run_all against correct runtime library

--- CFIXX-v2-Suite/run_all.sh
+++ CFIXX-v2-Suite/run_all.sh
@@ -1,4 +1,4 @@
-../release-install/bin/clang++ src/main_all.cpp src/attack_dynamic_dispatch.cpp src/attack_dynamic_cast.cpp src/attack_typeid.cpp -o build/cfixx_clang_all -O2 -g -std=c++11
+../release-install/bin/clang++ src/main_all.cpp src/attack_dynamic_dispatch.cpp src/attack_dynamic_cast.cpp src/attack_typeid.cpp -o build/cfixx_clang_all -O2 -g -std=c++11 -v -fuse-ld=gold -stdlib=libc++
 
 if [ $? -eq 0 ]
 then
